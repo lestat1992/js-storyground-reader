9459975914856a6a34926ea7f319e2fc5366f7de
lestat1992 <baldaccimassimiliano@gmail.com>
2021-12-28 10:23:20 +0100

    libreria funzionante

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import { createApp } from "vue";
 import { game } from "vue-storyground-reader";
 
-let app = {
-  name: "Test",
-  components: {
-    game,
-  },
-  template: `
-      <game/>
+Element.prototype.StoryGround = function (objSettings) {
+  let app = {
+    name: "StoryGround",
+    data() {
+      return {
+        objSettings: objSettings,
+      };
+    },
+    components: {
+      game,
+    },
+    template: `
+      <game 
+        v-bind:propLang="objSettings.propLang"
+        v-bind:langEditor="objSettings.langEditor"
+        v-bind:strings="objSettings.strings"
+        v-bind:gameData="objSettings.gameData"
+      />
     `,
-};
+  };
 
-createApp(app).mount("#app-test");
+  createApp(app).mount(this);
+};
