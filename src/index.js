8210916f2c2c3ec5e2649ade637de46449365b13
lestat1992 <baldaccimassimiliano@gmail.com>
2023-06-04 11:05:37 +0200

    !!! BUILD !!!

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import { createApp } from "vue";
 import { game } from "vue-storyground-reader";
 
 Element.prototype.StoryGround = function (objSettings = {}) {
-  let app = {
-    name: "StoryGround",
-    data() {
-      return {
-        objSettings: objSettings,
-        idStory: false,
-      };
-    },
-    components: {
-      game,
-    },
-    template: `
+    let app = {
+        name: "StoryGround",
+        data() {
+            return {
+                objSettings: objSettings,
+                idStory: false,
+                dataToEmit: null,
+            };
+        },
+        components: {
+            game,
+        },
+        template: `
       <game 
         v-bind:idStory="objSettings.idStory"
         v-bind:editorUsage="objSettings.editorUsage"
@@ -30,46 +31,60 @@ Element.prototype.StoryGround = function (objSettings = {}) {
         v-bind:useTheme="objSettings.useTheme"
         v-bind:canEmit="objSettings.canEmit"
         v-bind:showToast="objSettings.showToast"
-        v-on:emitByNodes="objSettings.emitByNodes ? objSettings.emitByNodes() : false"
+        v-on:emitByNodes="objSettings.emitByNodes ?  objSettings.emitByNodes(dataToEmit)  : false"
         v-on:onInit="objSettings.OnInit ? objSettings.OnInit() : false"
         v-on:beforeNavigation = " objSettings.beforeNavigation ? objSettings.beforeNavigation() : false "
         v-on:afterNavigation = " objSettings.afterNavigation ?  objSettings.afterNavigation() : false "
+        v-on:setDataToEmit="setDataToEmit"
       />
     `,
-    getPlayerValues: () => {
-      let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
+        methods: {
+            //pass emit data
+            setDataToEmit(ev) {
+                this.dataToEmit = ev;
+            },
+        },
 
-      window.dispatchEvent(new Event("getPlayerValues" + idStory));
-      let value = JSON.parse(localStorage["sg1Storage" + idStory]);
-      localStorage.removeItem("sg1Storage" + idStory);
-      return value;
-    },
-    getCurrentNodesValues: () => {
-      let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
+        getPlayerValues: () => {
+            let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
 
-      window.dispatchEvent(new Event("getCurrentNodesValues" + idStory));
-      let value = JSON.parse(localStorage["sg1Storage" + idStory]);
-      localStorage.removeItem("sg1Storage" + idStory);
-      return value;
-    },
-    //eventi set
-    setStartNode: (params) => {
-      let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
+            window.dispatchEvent(new Event("getPlayerValues" + idStory));
+            let value = JSON.parse(localStorage["sg1Storage" + idStory]);
+            localStorage.removeItem("sg1Storage" + idStory);
+            return value;
+        },
+        getCurrentNodesValues: () => {
+            let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
 
-      localStorage.setItem("sg1Storage" + idStory, JSON.stringify(params));
-      window.dispatchEvent(new Event("setStartNode" + idStory));
-    },
-    setPlayerValues: (params) => {
-      let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
+            window.dispatchEvent(new Event("getCurrentNodesValues" + idStory));
+            let value = JSON.parse(localStorage["sg1Storage" + idStory]);
+            localStorage.removeItem("sg1Storage" + idStory);
+            return value;
+        },
+        //eventi set
+        setStartNode: (params) => {
+            let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
 
-      localStorage.setItem("sg1Storage" + idStory, JSON.stringify(params));
-      window.dispatchEvent(new Event("setPlayerValues" + idStory));
-    },
-  };
+            localStorage.setItem(
+                "sg1Storage" + idStory,
+                JSON.stringify(params)
+            );
+            window.dispatchEvent(new Event("setStartNode" + idStory));
+        },
+        setPlayerValues: (params) => {
+            let idStory = this.childNodes[0].getAttribute("sg1-id-stroy");
 
-  createApp(app).mount(this);
+            localStorage.setItem(
+                "sg1Storage" + idStory,
+                JSON.stringify(params)
+            );
+            window.dispatchEvent(new Event("setPlayerValues" + idStory));
+        },
+    };
 
-  return app;
+    createApp(app).mount(this);
+
+    return app;
 };
 
 export {};
